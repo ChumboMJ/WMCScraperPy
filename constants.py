74b37c654d84dbc970d3e808e30994d09e0a3c52
ChumboMJ <tforste@gmail.com>
2024-12-30 21:19:04 -0800

    Create another Constant that Represents the header that would be sent if the request was from a browser

--- constants.py
+++ constants.py
@@ -11,3 +11,8 @@ RECORD_ATTRIBUTES = {
     ('rawPosition', 'classPosition', 'class', 'number', 'driver', 'car', 'rawTime', 'diff', 'fromFirst') : 2,
     ('position', 'class', 'number', 'driver', 'car', 'color', 'run1', 'run2', 'run3', 'run4', 'run5', 'run6', 'total', 'diff') : 3
 }
+
+# Represents the header that would be sent if the request was from a browser
+BROWSER_REQ_HEADER = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
