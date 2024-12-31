bfc99ca0b67ef28336ef89172f4043a4bd02ee44
ChumboMJ <tforste@gmail.com>
2024-12-30 21:36:28 -0800

    Create and open_url function with the proper error handling

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,4 +1,20 @@
+import requests
+from requests.exceptions import HTTPError
 from .constants import RESULTS_TYPE, RECORD_ATTRIBUTES, BROWSER_REQ_HEADER
 
 
-def process_wmc_url(result_type, attribute_names)
+def process_wmc_url(result_type, attribute_names):
+    return "Hello World"
+
+def open_url(url):
+    try:
+        #Send a GET request to the given url
+        response = requests.get(url, headers=BROWSER_REQ_HEADER)
+        # Raise an HTTPError for bad responses (4xx and 5xx)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        raise HTTPError(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        raise Exception(f"Other error occurred: {err}")
+    
+    return response
