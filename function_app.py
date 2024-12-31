0be4ebbcb2dad112737a7e34a089d69067ce1327
ChumboMJ <tforste@gmail.com>
2024-12-30 20:36:36 -0800

    Set up the function to return an Http Response with status code 400 if the request body is not valid JSON

--- function_app.py
+++ function_app.py
@@ -9,14 +9,15 @@ app = func.FunctionApp()
 def ScrapeWmcData(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        return func.HttpResponse(
+            "Invalid JSON in request body",
+            status_code=400
+        )
+    else:
+        name = req_body.get('name')
 
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
