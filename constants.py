22059f25c64d0560885eb785d1aca4aaabb00c9c
ChumboMJ <tforste@gmail.com>
2024-12-30 21:18:28 -0800

    Create a Dictionary that maps the Record attributes to the results types

--- constants.py
+++ constants.py
@@ -1,6 +1,13 @@
-# A Dctionary representing the three types of results
+# A Dictionary representing the three types of results
 RESULTS_TYPE = {
     "PAX" : 1,
     "RAW" : 2,
     "LAP" : 3
+}
+
+# A Dictionary representing the possible attributes from the data tables.
+RECORD_ATTRIBUTES = {
+    ('paxPosition', 'classPosition', 'class', 'number', 'driver', 'car', 'total', 'factor', 'paxTime', 'diff', 'fromFirst') : 1,
+    ('rawPosition', 'classPosition', 'class', 'number', 'driver', 'car', 'rawTime', 'diff', 'fromFirst') : 2,
+    ('position', 'class', 'number', 'driver', 'car', 'color', 'run1', 'run2', 'run3', 'run4', 'run5', 'run6', 'total', 'diff') : 3
 }
