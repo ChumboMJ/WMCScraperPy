2f0806e9626b0fb4600635f5fbb191ee285d2e11
ChumboMJ <tforste@gmail.com>
2025-01-03 23:03:18 -0800

    Add some TODOS to indicate where the code is going

--- function_app.py
+++ function_app.py
@@ -12,6 +12,8 @@ def ScrapeWmcData(req: func.HttpRequest) -> func.HttpResponse:
     # Retrieve the 'results_type' query parameter from the request URL
     name = req.params.get('results_type')
 
+    # TODO: Validate that the results_type is in the RESULTS_TYPE constant
+
     try:
         req_body = req.get_json()
     except ValueError:
@@ -23,6 +25,9 @@ def ScrapeWmcData(req: func.HttpRequest) -> func.HttpResponse:
         target_link = req_body.get('target_link')
 
     if target_link:
+        # TODO: Process the link to get the Tsble JSON
+        # TODO: Write the JSON data out to CosmosDB
+
         return func.HttpResponse(f"Here is the target_link value: {target_link}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
